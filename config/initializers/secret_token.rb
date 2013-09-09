71baa38208db5d09482663537ef6a2e7370d6ff1
Allegra Larche <allegralarche@gmail.com>
2013-09-09 19:41:56 -0400

    added initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstRailsApp::Application.config.secret_key_base = '01d33bd6dffbab0ca12cb2a88afa473ee0e0462dc85c6516cb426aa36b54e664b68159742129e75a0e729f421081113b7850e532a885c7a8018b68ec45ee0669'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstRailsApp::Application.config.session_store :cookie_store, key: '_firstRailsApp_session'
